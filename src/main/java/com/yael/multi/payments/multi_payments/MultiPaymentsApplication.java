372ddb3963b9fca9382e92be7c5a3a92a1544059
YaelDev <jorge.yael333@gmail.com>
2025-11-15 17:55:57 -0600

    First commit

---
+++ src/main/java/com/yael/multi/payments/multi_payments/MultiPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.yael.multi.payments.multi_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yael/multi/payments/multi_payments/MultiPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yael.multi.payments.multi_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
